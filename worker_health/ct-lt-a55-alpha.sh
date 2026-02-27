a5227f9b535b41d4204e807dd21f51bcd17b49f6
Andrew Erickson <aerickson@mozilla.com>
2026-02-27 13:49:44 -0800

    helper script work

--- worker_health/ct-lt-a55-alpha.sh
+++ worker_health/ct-lt-a55-alpha.sh
@@ -3,6 +3,8 @@
 set -e
 # set -x
 
+PROV_AND_WORKER_TYPE="proj-autophone/gecko-t-lambda-alpha-a55"
+
 # check that count argument is provided
 if [ -z "$1" ]; then
     echo "Usage: $0 <count> [additional arguments]"
@@ -12,4 +14,4 @@ fi
 COUNT="$1"
 shift
 
-./create_tc_task.py -q releng-hardware/gecko-t-linux-talos-2404 -c "$COUNT" "$@"
+pipenv run ./create_tc_task.py -q "$PROV_AND_WORKER_TYPE" -c "$COUNT" "$@"
---
+++ worker_health/ct-talos-2404.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+# set -x
+
+PROV_AND_WORKER_TYPE="releng-hardware/gecko-t-linux-talos-2404"
+
+# check that count argument is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <count> [additional arguments]"
+    exit 1
+fi
+
+COUNT="$1"
+shift
+
+pipenv run ./create_tc_task.py -q "$PROV_AND_WORKER_TYPE" -c "$COUNT" "$@"
