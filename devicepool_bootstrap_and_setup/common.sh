a09914a3cffca36050ac153a64cea35835a76ee2
Andrew Erickson <aerickson@mozilla.com>
2026-05-04 12:38:27 -0700

    devicepool scripts: update for lt and v3 bb servers

--- devicepool_bootstrap_and_setup/common.sh
+++ devicepool_bootstrap_and_setup/common.sh
@@ -5,6 +5,8 @@ set -e
 # local paths
 export BOOTSTRAP_SCRIPT_PATH="$HOME/git/ronin_puppet/provisioners/linux/bootstrap_bitbar_devicepool.sh"
 export BITBAR_ENV_FILE_PATH="$HOME/git/mozilla-bitbar-devicepool/bitbar_env.sh"
+export BITBAR_V3_ENV_FILE_PATH="$HOME/git/mozilla-bitbar-devicepool/bitbar_env-v3-server.sh"
+export LT_ENV_FILE_PATH="$HOME/git/mozilla-bitbar-devicepool/lt_env.sh"
 export BITBAR_FILES_PATH="$HOME/git/mozilla-bitbar-devicepool/files/relops*"
 export PATH_TO_SOPS_REPO="$HOME/git/sops"
 
@@ -14,3 +16,5 @@ export LAST_STARTED_SERVICE_FILE='/etc/systemd/system/bitbar-last_started_alert.
 export SLACK_ALERT_CONFIG_FILE='/home/bitbar/.bitbar_slack_alert.toml'
 export INFLUX_LOGGER_CONFIG_FILE='/home/bitbar/.bitbar_influx_logger.toml'
 export BITBAR_ENV_FILE='/etc/bitbar/bitbar.env'
+export BITBAR_V3_ENV_FILE='/etc/bitbar/bitbar-v3.env'
+export LT_ENV_FILE='/etc/bitbar/lambdatest.env'
--- devicepool_bootstrap_and_setup/distribute_bitbar_env.sh
+++ devicepool_bootstrap_and_setup/distribute_bitbar_env.sh
@@ -15,3 +15,9 @@ set -x
 # TODO: grab this from SOPS vs local clone of devicepool
 scp "$BITBAR_ENV_FILE_PATH" "$the_host":bitbar.env
 ssh "$the_host" 'sudo mv bitbar.env /etc/bitbar/ && sudo chown root:bitbar /etc/bitbar/bitbar.env && sudo chmod 660 /etc/bitbar/bitbar.env'
+
+scp "$BITBAR_V3_ENV_FILE_PATH" "$the_host":bitbar-v3.env
+ssh "$the_host" 'sudo mv bitbar-v3.env /etc/bitbar/ && sudo chown root:bitbar /etc/bitbar/bitbar-v3.env && sudo chmod 660 /etc/bitbar/bitbar-v3.env'
+
+scp "$LT_ENV_FILE_PATH" "$the_host":lambdatest.env
+ssh "$the_host" 'sudo mv lambdatest.env /etc/bitbar/ && sudo chown root:bitbar /etc/bitbar/lambdatest.env && sudo chmod 660 /etc/bitbar/lambdatest.env'
